a9611aab1eb3924c949b5d1edda0342c734182c5
a.23308051280663 <a.23308051280663@cbtis128.edu.mx>
2026-02-12 19:14:35 +0000

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,103 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(AppSimilares());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class AppSimilares extends StatelessWidget {
+  const AppSimilares({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return MaterialApp(title: 'Productos del similares', home: Productos());
+  }
+}
+
+class Productos extends StatelessWidget {
+  const Productos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        centerTitle: true,
+        title: const Text(
+          'SIMILARES de Ochoa',
+          style: TextStyle(color: Color.fromARGB(255, 9, 39, 36), fontWeight: FontWeight.bold),
         ),
+        backgroundColor: const Color.fromARGB(255, 122, 201, 193),
+        elevation: 4,
+        leading: const Icon(Icons.local_pharmacy, color: Colors.white),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.medication),
+            color: const Color.fromRGBO(22, 66, 63, 0.702),
+            onPressed: () {},
+            tooltip: 'Medicamento',
+          ),
+          IconButton(
+            icon: const Icon(Icons.local_shipping),
+            color: const Color.fromRGBO(22, 66, 63, 0.702),
+            onPressed: () {},
+            tooltip: 'Proveedor',
+          ),
+          IconButton(
+            icon: const Icon(Icons.point_of_sale),
+            color: const Color.fromRGBO(22, 66, 63, 0.702),
+            onPressed: () {},
+            tooltip: 'Venta',
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+ 
+    body: ListView(
+  padding: const EdgeInsets.all(10), // Espacio alrededor de la lista
+  children: [
+    ListTile(
+      leading: const Icon(Icons.star, color: Colors.white),
+      title: const Text('Peluche', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+      subtitle: const Text('Peluche del dr simi', style: TextStyle(color: Colors.white70)),
+      trailing: const Icon(Icons.arrow_forward_ios, color: Colors.white, size: 15),
+      tileColor: Colors.deepPurpleAccent,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    ),
+    const SizedBox(height: 10), // Espacio entre elementos
+    ListTile(
+      leading: const Icon(Icons.favorite, color: Colors.white),
+      title: const Text('Libro para colorear', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+      subtitle: const Text('SLibro para coloreas del dr simi', style: TextStyle(color: Colors.white70)),
+      trailing: const Icon(Icons.arrow_forward_ios, color: Colors.white, size: 15),
+      tileColor: Colors.pinkAccent,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    ),
+    const SizedBox(height: 10),
+    ListTile(
+      leading: const Icon(Icons.flash_on, color: Colors.white),
+      title: const Text('Calcetines', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+      subtitle: const Text('Calcetines con el rostro del dr simi', style: TextStyle(color: Colors.white70)),
+      trailing: const Icon(Icons.arrow_forward_ios, color: Colors.white, size: 15),
+      tileColor: Colors.orangeAccent,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    ),
+    const SizedBox(height: 10),
+    ListTile(
+      leading: const Icon(Icons.rocket_launch, color: Colors.white),
+      title: const Text('Taza del dr simi', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+      subtitle: const Text('Taza con la imagen del dr simi', style: TextStyle(color: Colors.white70)),
+      trailing: const Icon(Icons.arrow_forward_ios, color: Colors.white, size: 15),
+      tileColor: Colors.cyan,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    ),
+    const SizedBox(height: 10),
+    ListTile(
+      leading: const Icon(Icons.celebration, color: Colors.white),
+      title: const Text('LLavero del dr simi', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+      subtitle: const Text('LLavero con argolla con la imagen del dr simi', style: TextStyle(color: Colors.white70)),
+      trailing: const Icon(Icons.arrow_forward_ios, color: Colors.white, size: 15),
+      tileColor: Colors.lightGreenAccent.shade700,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    ),
+  ],
+)
+ 
     );
   }
 }
